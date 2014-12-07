7caabcb492ad71a0a27249523282c90afa39cc39
Anirudh Subramanian <anirudh2290@gmail.com>
2014-12-07 11:36:55 -0500

    Add comments

--- fuserpc_edited.py
+++ fuserpc_edited.py
@@ -21,7 +21,11 @@ if not hasattr(__builtins__, 'bytes'):
 
 class Memory(LoggingMixIn, Operations):
     'Example memory filesystem. Supports only one level of files.'
-    
+   
+    """
+	transform extracts the parent path from the current path
+	So if current path is /mtr/tp/tp1 parent path will be /mtr/tp
+    """ 
 
     def transform(self,s):
         if s == '/':
@@ -37,6 +41,7 @@ class Memory(LoggingMixIn, Operations):
             hiepath = '/'        
         return hiepath
 
+
     def ltransform(self,s):
         plindex = len(s) - 1
         while plindex > 0:
@@ -46,6 +51,11 @@ class Memory(LoggingMixIn, Operations):
         hielpath = s[(plindex)+1:len(s)]
         return hielpath
 
+    """
+	ltransform extracts current folder or filename from full path 
+	name that goes into the key. Therefore if current path is
+	/mtr/tp/tp1 the output will be tp1
+    """
     def ltransformparent(self, s, parent):
         if parent == '/':
             return s[1:]        
@@ -53,6 +63,12 @@ class Memory(LoggingMixIn, Operations):
         px = str(parent) + '/'
         if(sx.startswith(px)):
             return s[len(px):]
+
+    """
+	Passed in the memcacheht object which will be the backend
+	memcached hashtable that will be used
+		
+    """	
 
     def __init__(self, distributedServer):
         self.files = distributedServer
@@ -62,6 +78,10 @@ class Memory(LoggingMixIn, Operations):
         #self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2, files_folders=Set(['/']))
         self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2, files_folders=Set(['/']), hpath = '/')  #Modified for hierarchical FS
 
+    """	
+	chmod retrieves the hash table and modifies the mode for the files
+    """
+
     def chmod(self, path, mode):
         ht = self.files[path]
         ht['st_mode'] &= 0770000
@@ -70,13 +90,23 @@ class Memory(LoggingMixIn, Operations):
         #self.files[path]['st_mode'] &= 0770000
         #self.files[path]['st_mode'] |= mode
         return 0
-
+    
+    """
+	chown retrieves the hash table and changes the user id and group id and writes
+	it back to the hash table
+    """
     def chown(self, path, uid, gid):
         ht = self.files[path]
         ht['st_uid'] = uid
         ht['st_gid'] = gid
         self.files[path] = ht
      
+    """
+	creates the key value pair for the new file. The value will be the metadata dict
+	and the files_folders key inside the value which stores contents will be initially
+	blank
+    """
+
     def create(self, path, mode):
         #self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
         #                        st_size=0, st_ctime=time(), st_mtime=time(),
@@ -94,7 +124,12 @@ class Memory(LoggingMixIn, Operations):
         self.files[hp] = ht #Modified for hierarchical FS
         self.fd += 1
         return self.fd
-
+    
+    """
+	getattr retrieves the dict and retrieves the attr key inside the 
+	value
+    """
+    	
     def getattr(self, path, fh=None):
         hpath = self.transform(path)
         ht = self.files[hpath]
@@ -103,6 +138,9 @@ class Memory(LoggingMixIn, Operations):
 
         return self.files[path]
 
+    """
+	getxattr retrieves the custom defined attr which retrieves the value corresponding to the key - name
+    """
     def getxattr(self, path, name, position=0):
         attrs = self.files[path].get('attrs', {})
 
@@ -111,10 +149,18 @@ class Memory(LoggingMixIn, Operations):
         except KeyError:
             return ''       # Should return ENOATTR
 
+    """
+	lists all custom defined attributes
+    """
     def listxattr(self, path):
         attrs = self.files[path].get('attrs', {})
         return attrs.keys()
 
+    """
+	mkdir creates a dict value corresponding to the key path.
+	modifications made to the link numbers and files_folders
+	of the parent path
+    """
     def mkdir(self, path, mode):
         #self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
         #                        st_size=0, st_ctime=time(), st_mtime=time(),
@@ -139,11 +185,18 @@ class Memory(LoggingMixIn, Operations):
         self.fd += 1
         return self.fd
 
+    """
+	reads the file
+    """
     def read(self, path, size, offset, fh):
         ht = self.files[path]
         if 'files_folders' in ht:
             return ht['files_folders'][offset:offset + size]
         return None
+
+    """
+        traverses through the files_folders to read contents of the folder
+    """
 
     def readdir(self, path, fh):
         #return ['.', '..'] + [x[1:] for x in self.files['/']['files_folders'] if x != '/']
@@ -151,9 +204,17 @@ class Memory(LoggingMixIn, Operations):
         #return ['.', '..'] + [x[1:] for x in self.files[hp]['files_folders'] if x != '/' or x != hp ] #Modified for hierarchical FS 
         return ['.', '..'] + [self.ltransformparent(x, path) for x in self.files[path]['files_folders'] if (self.ltransformparent(x, path) != '' and self.ltransformparent(x, path) != None)] #Modified for hierarchical FS 
 
+
+    
+
     def readlink(self, path):
         return self.files[path]['files_folders']
 
+    """
+	Removes custom defined attributes.
+	removes attr corresponding to key name in attrs dict inside the metadata
+	
+    """
     def removexattr(self, path, name):
         attrs = self.files[path].get('attrs', {})
         try:
@@ -163,6 +224,11 @@ class Memory(LoggingMixIn, Operations):
 	    self.files[path] = ht	
         except KeyError:
             pass        # Should return ENOATTR
+
+    """
+	renames files_folders associates files_folders with new and
+	removes old
+    """
 
     def rename(self, old, new):
         temp = self.files[old]
@@ -178,6 +244,12 @@ class Memory(LoggingMixIn, Operations):
         self.files[hp] = ht
         self.files[new1] = htnew1
 
+    """
+	Removes the folder corresponding to the path
+	by removing links from parent path and then
+	deleting the key from the hash table
+    """
+
     def rmdir(self, path):
         #self.files.pop(path)
         hpath = self.transform(path)
@@ -190,6 +262,10 @@ class Memory(LoggingMixIn, Operations):
         self.files[hpath] = ht #Modified for hierarchical FS 
 
 
+    """
+	sets custom defined attribute corresponding to the key "name"
+    """
+
     def setxattr(self, path, name, value, options, position=0):
         # Ignore options
         ht = self.files[path]
@@ -198,9 +274,17 @@ class Memory(LoggingMixIn, Operations):
         ht['attrs'] = attrs
         self.files[path] = ht
 
+    """
+	outputs stats for the directory including size no of links,
+	permissions etc. 
+    """
     def statfs(self, path):
         return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
 
+    """
+	symlink copies the files_folders of the source into the new 
+	metadata of the target
+    """
     def symlink(self, target, source):
         'creates a symlink `target -> source` (e.g. ln -s source target)'
 	hp = self.transform(target)
@@ -222,12 +306,19 @@ class Memory(LoggingMixIn, Operations):
         self.files[httarget1] = httarget1
 	"""
 
+    """
+	truncates upto length of a file
+    """
     def truncate(self, path, length, fh=None):
         ht = self.files[path]
         if 'files_folders' in ht:
             ht['files_folders'] = ht['files_folders'][:length]
         ht['st_size'] = length
         self.files[path] = ht
+
+    """
+	unlink key value corresponding to the path
+    """
 
     def unlink(self, path):
         #self.files.pop(path)
@@ -237,12 +328,20 @@ class Memory(LoggingMixIn, Operations):
         self.files[hpath] = ht
         del self.files[path]
 
+    """
+	Modifies the atime and mtime  corresponding to the
+	path key 
+    """
     def utimens(self, path, times=None):
         now = time()
         atime, mtime = times if times else (now, now)
         self.files[path]['st_atime'] = atime
         self.files[path]['st_mtime'] = mtime
 
+    """
+	writes the data to a file by modifying the value corresponding to files_folders
+    """
+    
     def write(self, path, data, offset, fh):
         """
         self.data[path] = self.data[path][:offset] + data
